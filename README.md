5212e4c13bb100726f1fde6b57b28854ab5ce8c3
lovez <bismih@qq.com>
2026-03-02 23:49:21 +0800

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # view_hands
-<<<<<<< HEAD
-# view_hands
-=======
->>>>>>> c2030de7253826e538b809daa4d7d0875770c33e
---
+++ git.sh
@@ -0,0 +1,5 @@
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:lizi-learn/view_hands.git
+git push -u origin main
